a9851edd23d8d1ed8a28865ac61db0ae927146e6
Cheng Chen <chengchen246@gmail.com>
2024-09-27 00:53:41 +0000

    renamed columns

--- models/staging/super_store_analysis/stg_super_store_analysis__Orders.sql
+++ models/staging/super_store_analysis/stg_super_store_analysis__Orders.sql
@@ -14,8 +14,16 @@ source as (
 renamed as (
 
     select
-       *
-
+         'Order ID' as Order_id
+        ,'Order Date' as order_date
+        , 'Ship Date' as ship_date
+        , 'Ship Mode' as ship_mode
+        , 'Customer ID' as customer_id
+        , 'Segment' as segment
+        , 'Country' as country
+        , 'Product ID' as product_id
+        , 'Discount' as discount
+        , 'Profit' as profit
     from source
 
 )
